266aff3ca481af371e6122010694b78faa4d11df
artsvet <61990390+artsvet@users.noreply.github.com>
2020-09-30 11:58:48 -0400

    Cleaned and commented.

--- lipidGen.py
+++ lipidGen.py
@@ -1,12 +1,50 @@
 import os
 import re
 import pandas as pd
+import pathlib
+import threading
+from pymol import cmd
+from openbabel import pybel
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.Chem import Draw
-from openbabel import pybel
 
-plPatterns = {
+
+class SafeThreads:
+    """
+    Performs parallel thread-safe execution
+    of a function using a Semaphore
+    on a list of arguments consumed to a Queue,
+    for cleaning and conversion of Protein files.
+    """
+
+    def __init__(self, function, sema_buffer=4, q_size=0):
+        self.function = function
+        self.sema = threading.BoundedSemaphore(sema_buffer)
+        self.q = Queue(maxsize=q_size)
+        self.threads = []
+
+    def wrap(self, arg):
+        self.sema.acquire()
+        print('In worker thread: ',
+              "\n", arg,
+              "\n", threading.current_thread()
+        )
+        self.function(arg)
+        self.sema.release()
+        print('done working: ', threading.current_thread())
+
+    def __call__(self, *args, **kwargs):
+        consume(self.q.put(arg) for arg in args)
+        with self.sema:
+            while self.q.qsize() > 0:
+                t = threading.Thread(target=self.wrap, args=(self.q.get(),))
+                t.start()
+                self.threads = threading.enumerate()
+
+
+PL_PATTERNS = {
+    # template pattern for phospholipid molecule classes
     'pa':'CC(COP(=O)(O)O)O',
     'pc':'CC(COP(=O)([O-])OCC[N+](C)(C)C)O',
     'pe':'CC(COP(=O)(O)OCCN)O',
@@ -18,46 +56,133 @@ plPatterns = {
     }
 
 
-class Mol():
+class Protein(type(pathlib.Path())):
+    """
+    Protein class instantiated using path
+    to .pdb, .pdbqt, or other structure file.
+    Implements methods to clean and convert
+    structure files, with thread-safe parallel
+    execution if used with SafeThreads.
+    """
 
-    def toChem(self):
+    def __init__(self, path):
+
+        super().__init__()
+        
+    def clean(self):
+
+        if '.clean' in self.suffixes:
+            return self
+        else:
+            self.path_clean = Protein(
+                self.with_name(
+                    self.stem + '.clean' + self.suffix
+                )
+            )
+            with open(self, "r") as f:
+                lines = f.readlines()
+            with open(self.path_clean, "w") as f:
+                for line in lines:
+                    if line.strip("\n")[:4] == "ATOM":
+                        f.write(line)
+                f.close()
+
+    def convert(self):
+
+        if '.pdbqt' in self.suffixes:
+            return self
+        else:
+            self.path_pdbqt = Protein(self.with_suffix('.pdbqt'))
+            cmd.load(self.__str__())
+            cmd.remove('resn HOH')
+            cmd.h_add(selection='acceptors or donors')
+            cmd.save(self.__str__())
+            mols = list(pybel.readfile('pdb', self.path_clean.__str__()))
+            writer = pybel.Outputfile(
+                'pdbqt', self.path_pdbqt.__str__(), opt={'pdbqt': '-xh'}
+            )
+            for molecule in mols:
+                writer.write(molecule)
+                writer.close()
+
+    def prepare(self):
+
+        if hasattr(self, 'path_pdbqt'):
+            self.path_pdbqt.clean()
+        elif hasattr(self, 'path_clean'):
+            self.path_clean.convert().clean()
+        else:
+            self.clean().convert().clean()
+
+class Mol:
+    """
+    Base class for ligand molecules,
+    takes SMILES formatted string representatons
+    and implements interface to Rdkit and
+    Openbabel to generate, convert,
+    and display molecules.
+    """
+
+    def to_chem(self):
+
         return Chem.MolFromSmiles(self.__str__())
 
-    def showMol(self):
+    def show_mol(self):
+
         return Draw.ShowMol(Chem.MolFromSmiles(self.__str__()), size=(600, 300))
 
-    def toImage(self):
+    def to_image(self):
+
         return Draw.MolToImage(self.__str__(), size=(600, 300))
 
-    def toPybel(self):
+    def to_pybel(self):
+
         return pybel.readstring('smi', self.__str__())
 
-    def write3dPdb(self):
+    def write_pdb(self):
+
             target = os.path.join(
                 os.getcwd(), self.type
             )
-
             os.makedirs(os.path.basename(target), exist_ok=True)
             os.chdir(target)
-            x = self.toChem()
+            x = self.to_chem()
             x = Chem.AddHs(x)
             AllChem.EmbedMolecule(x, AllChem.ETKDG())
             AllChem.UFFOptimizeMolecule(x, 1000)
             AllChem.MolToPDBFile(x, self.name + '.pdb')
 
+    def __str__(self):
+
+        return self.construct()
+
+    def __repr__(self):
+
+        return self.construct()
+
 class Lipid(Mol):
+    """
+    Lipid class, generating a string
+    representation of carbon chains with variable
+    length, double-bonding, and acidity.
+    ex - Lipid(8, E=[3,6]) : C/C=C/C/C=C/CC(=O)O
+    """
 
     def __init__(self, len, E=[], Z=[], acid=True):
-        self.name = str(len) + 'E' + str(E) + 'Z' + str(Z)
+
         self.len = len
-        self.E = E
-        self.Z = Z
+        self.E, self.Z = E, Z
         self.acid = acid
         if self.acid:
             self.type = 'fatty acid'
         else:
             self.type = 'lipid'
+        self.name = str(self.len) \
+                    + 'E' + str(self.E) \
+                    + 'Z' + str(self.Z)
+
     def construct(self):
+
         chain = 'C' * self.len
 
         for i in sorted(self.E, reverse=True):
@@ -76,51 +201,67 @@ class Lipid(Mol):
         else:
             return chain[::-1]
 
-    def __str__(self):
-        return self.construct()
+class Lea(Mol):
+    """
+    Synthetic Lea drug class,
+    generating a string representation using
+    LEA template backbone and r1 Lipid.
+    ex - Lea(Lipid(8, E=[3,6])) : C/C=C/C/C=C/CCOCC(CNC(C)C)O
+    """
 
-class LeaFactory(Mol):
-    '''
-    Parametrizes LEA structure generation
-    '''
     def __init__(self, r1):
+
+        self.r1 = r1
         self.type = 'LEA'
-        self.r1 = r1.__str__().replace('(=O)O','')
-
-    def __str__(self):
-        return str(self.r1 + 'OCC(CNC(C)C)O')
-
-class PlFactory(Mol):
-
-    def __init__(self, pattern, r1='', r2=''):
-        self.type = 'phospholipid'
-        self.pattern = pattern
-        self.r1 = r1.__str__()
-        self.r2 = r2.__str__()[::-1].replace('O)O=(C', 'C(=O)')
-
+        self.name = 'LEA' + self.r1.name
 
     def construct(self):
-        if self.pattern == 'sm' or self.pattern == 'cm':
-            self.r1 = 'CCCCCCCCCCCCCC=CC'
-            return str(self.r1
-                       + plPatterns[self.pattern]
-                       + self.r2
-                       + ')O'
-                       )
 
+        return self.r1.__str__().replace('(=O)O','') \
+            + 'OCC(CNC(C)C)O'
+
+class Pl(Mol):
+    """
+    Phospholipid compound class,
+    generating a string representation using
+    pattern backbone dict, r1, r2 lipid side chain
+    """
+
+    def __init__(self, pattern, r1='', r2=''):
+        self.pattern = pattern
+        if r2:
+            self.type = 'phospholipid'
         else:
-            return str(self.r1
-                       + plPatterns[self.pattern]
-                       + self.r2
-                       )
+            self.type = 'lysoPl'
+        if self.pattern == 'sm' or self.pattern == 'cm':
+            self.r1 = Lipid(16, E=[2], acid=False)
+        else:
+            self.r1 = r1
+        self.r2 = r2
+        self.name = pattern \
+                    + self.r1.name \
+                    + getattr(self.r2, name, default='')
 
-    def __str__(self):
-        return self.construct()
+    def construct(self):
+
+        structure = self.r1.__str__() \
+            + PL_PATTERNS[self.pattern] \
+            + self.r2.__str__()[::-1].replace(
+                'O)O=(C', 'C(=O)'
+            )
+
+        if pattern == 'sm' or self.pattern == 'cm':
+            structure = structure + ')O'
+
+        return structure
 
 class LipidFromSeries(Lipid):
+    """
+    Parses series input to
+    generate Lipid objects
+    """
 
-    def __init__(self, series):
-        self.name = series['id']
+    def __init__(self, series, acid=True):
         self.len = series['length']
         self.E = [int(E)
                   for E in series['E'].split(',')
@@ -130,16 +271,26 @@ class LipidFromSeries(Lipid):
                   for Z in series['Z'].split(',')
                   if Z != ''
                   ]
-        self.acid = True
+        super().__init__(self.len, E=self.E, Z=self.Z, acid=acid)
 
-class LeaFromSeries(LeaFactory):
+
+class LeaFromSeries(Lea):
+    """
+    Parses series input to
+    generate LEA objects
+    """
 
     def __init__(self, series):
         self.name = series['id']
-        self.r1 = LipidFromSeries(series).__str__()
+        self.r1 = LipidFromSeries(series)
         super().__init__(self.r1)
 
-class PlFromSeries(PlFactory):
+
+class PlFromSeries(Pl):
+    """
+    Parses series input to
+    generate phospholipid objects
+    """
     def __init__(self, pattern, series, lipidTable):
         self.pattern = pattern
         self.name = pattern + ' ' + series['id']
@@ -147,7 +298,7 @@ class PlFromSeries(PlFactory):
             lipidTable.loc[
                 lipidTable['id'] == series['r1']
                 ].squeeze()
-            ).__str__()
+            )
         if series['r2']:
             self.r2 = LipidFromSeries(
                 lipidTable.loc[
@@ -158,7 +309,17 @@ class PlFromSeries(PlFactory):
             self.r2 = ''
         super().__init__(self.pattern, self.r1, self.r2)
 
+
 class targetsParse:
+    """
+    Parses Series to set up dependencies for ligand
+    docking. Contains Protein target attributes,
+    specifies ligands that will be docked using
+    reference lipid and pl tables. Will serve as
+    primary interface for running dock and
+    collecting outputs.
+    """
+
     def __init__(self, targetsSeries, lipidTable, plTable):
         self.lipidTable = lipidTable
         self.plTable = plTable
@@ -169,18 +330,19 @@ class targetsParse:
         self.function = targetsSeries['function']
         self.ligands = re.split(':|,', targetsSeries['lipid'])
         self.ligandsIndexes = self.ligands[1:]
+        self.box = targetsSeries['box']
 
-    def buildLeaDict(self):
+    def build_lea_dict(self):
         leaDict = {}
         for index, series in self.lipidTable.iterrows():
             leaDict[series['id']] = LeaFromSeries(series)
         return leaDict
 
-    def buildLigandDict(self):
+    def build_ligand_dict(self):
         ligandDict = {}
 
         if self.ligands[0] == 'all':
-            ligandPatterns = [key for key in plPatterns]
+            ligandPatterns = [key for key in PL_PATTERNS]
         else:
             ligandPatterns = [self.ligands[0]]
 
@@ -205,17 +367,17 @@ class targetsParse:
 
         return ligandDict
 
-    def writePdbs(self):
-        compiledLigands = {**self.buildLeaDict(), **self.buildLigandDict()}
+    def write_ligands(self):
+        ligands = {**self.build_lea_dict(), **self.build_ligand_dict()}
         root = os.getcwd()
-        pdbDir = os.path.join(
+        dir = os.path.join(
                 root, self.name, self.id
             )
-        os.makedirs(pdbDir)
-        os.chdir(pdbDir)
-        for ligand in compiledLigands:
-            compiledLigands[ligand].write3dPdb()
-            os.chdir(pdbDir)
+        os.makedirs(dir)
+        os.chdir(dir)
+        for ligand in ligands:
+            ligands[ligand].write_pdb()
+            os.chdir(dir)
         os.chdir(root)
 
 
